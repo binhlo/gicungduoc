96aee71c85d5c789de071d304782abc20bc4b406
binhlo <160563459+binhlo@users.noreply.github.com>
2024-03-19 08:23:51 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,228 @@
+
+
+/*SEARCH*/ 
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+/*HEADER*/
+window.onscroll = () =>{
+
+    searchForm.classList.remove('active');
+    
+    if(window.scrollY > 80) {
+        document.querySelector('.header ,header-2').classList.add('active');
+    }
+    else{
+        document.querySelector('.header ,header-2').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+  fadeOut();
+}
+
+
+/*LOGIN*/
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+
+
+
+
+/*FEATURED SECTION */
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+  /*CATEGORY*/
+  
+  var swiper = new Swiper(".category-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+
+
+
+
+
+
+
+  /*ARRIVAL SECTION*/
+  var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+
+
+  /*REVIEWS SECTION*/
+
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  /*BLOGS SECTION*/
+  var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+  /* LOADER */
+
+ function loader() {
+    document.querySelector('.loader-container').classList.add('active');
+  } 
+  
+  function fadeOut() {
+    setTimeout(loader, 3000);
+  } 
+
+  /* PHAN HOI */
+
+  let gui = () => {
+    alert("Thanks for telling us");
+  }
